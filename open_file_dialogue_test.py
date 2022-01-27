fe627cbc4e08b416a426bd40d1d32a63860a80eb
Raghav-Srinivasan0 <raghavsrinivasan0@gmail.com>
2022-01-26 22:23:11 -0500

    various changes made (WORKING) 1/26/2022

--- open_file_dialogue_test.py
+++ open_file_dialogue_test.py
@@ -1,8 +1,9 @@
-#this code works
+# this code works
 
 import os
 import tkinter as tk
 from tkinter import filedialog
+
 
 class FileDialogue:
     def __init__(self):
@@ -11,14 +12,19 @@ class FileDialogue:
 
         self.file_path = filedialog.askopenfilename()
 
-        self.file = open(self.file_path,"r+b")
+        self.file = open(self.file_path, "r+b")
+
     def get_file_path(self):
         return self.file_path
+
     def get_file_length(self):
         return os.path.getsize(self.file_path)
+
     def get_file_data(self):
         return self.file.readlines()
+
     def get_file_ext(self):
-        return self.file_path[self.file_path.index('.'):]
+        return self.file_path[self.file_path.index(".") :]
+
     def close(self):
         self.file.close()
--- socket_tutorial_echo_server.py
+++ socket_tutorial_echo_server.py
@@ -1,21 +1,42 @@
+from importlib.resources import path
 import socket
 
-HOST = '127.0.0.1'
-PORT = 5000
-PATH = f"C:/Users/binad/Peer-To-Peer File Storage/Peer-to-Peer-File-Storage/Test Destination/"
-FILENAME = input("What is the name of your file? ")
+from black import main
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    #conn is a socket object and addr is a pair with (hostaddr, port)
-    conn, addr = s.accept()
-    with conn:
-        #print('Connected to: ' + addr)
-        type_of_file = str(conn.recv(1024)).replace("'","").replace("b","")
-        file = open(PATH + FILENAME + type_of_file, "a+b")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            file.write(data)
+
+class Server:
+    def __init__(
+        self,
+        host="127.0.0.1",
+        port=5000,
+        path="C:/Users/binad/Peer-To-Peer File Storage/Peer-to-Peer-File-Storage/Test Destination/",
+    ):
+        self.HOST = host
+        self.PORT = port
+        self.PATH = path
+
+    def startserver(self, FILENAME=None):
+        if FILENAME == None:
+            FILENAME = str(input("What is the name of your file? "))
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((self.HOST, self.PORT))
+            s.listen()
+            # conn is a socket object and addr is a pair with (hostaddr, port)
+            conn, addr = s.accept()
+            with conn:
+                # print('Connected to: ' + addr)
+                type_of_file = str(conn.recv(1024)).replace("'", "").replace("b", "")
+                file = open(self.PATH + FILENAME + type_of_file, "a+b")
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    file.write(data)
+
+
+if __name__ == "__main__":
+    server = Server(
+        host="192.168.1.3", port=5000, path="D:/Destination_For_File-Transfer/"
+    )
+    while True:
+        server.startserver()
