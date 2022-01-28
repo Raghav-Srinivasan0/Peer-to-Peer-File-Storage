54f1eba3927fe8f1ded12bd3a3839677480c6001
Raghav-Srinivasan0 <raghavsrinivasan0@gmail.com>
2022-01-28 14:00:39 -0500

    multi-threading v2 (NOT WORKING)

--- multi_threading_server.py
+++ multi_threading_server.py
@@ -1,8 +1,16 @@
 from socket_basic_server import Server
 import threading
 
+HOST='localhost'
+PORT=5000
+PATH="C:/Users/binad/Peer-To-Peer File Storage/Peer-to-Peer-File-Storage/Test Destination/"
+
+def createandrun(filename):
+    server = Server(host=HOST,port=PORT,path=PATH)
+    server.startserver(filename=filename)
+
 while(True):
     # Close the connection to the client
-    thread = threading.Thread(target=Server.startserver, args=(str(threading.active_count())))
+    thread = threading.Thread(target=createandrun, args=(str(threading.active_count()),))
     thread.start()
     thread.run()
