3e80a37dabcdb6e488a327a692bfda4a79459909
Raghav-Srinivasan0 <raghavsrinivasan0@gmail.com>
2022-01-30 16:17:49 -0500

    modified for more than one server

--- socket_original_client.py
+++ socket_original_client.py
@@ -7,22 +7,29 @@ import argparse
 my_parser = argparse.ArgumentParser(description="Start the client")
 
 my_parser.add_argument("PORT", metavar="port", type=int, help="The port")
-my_parser.add_argument("HOST", metavar="host", type=str, help="The host")
+# my_parser.add_argument("HOST", metavar="host", type=str, help="The host")
 
 args = my_parser.parse_args()
 
-HOST = args.HOST
+hosts = [
+    "192.168.1.3"
+    # , "127.0.0.1"
+]
+
+# HOST = args.HOST
 PORT = args.PORT
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    f = FileDialogue()
-    file_path = f.get_file_path()
-    file_data = f.get_file_data()
-    file_ext = f.get_file_ext()
-    f.close()
-    os.chdir("C:")
-    os.remove(file_path)
-    s.sendall(bytes(file_ext.encode()))
-    for i in range(len(file_data)):
-        s.sendall(file_data[i])
+for host in range(len(hosts)):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((hosts[host], PORT))
+        f = FileDialogue()
+        file_path = f.get_file_path()
+        file_data = f.get_file_data()
+        file_ext = f.get_file_ext()
+        f.close()
+        os.chdir("C:")
+        os.remove(file_path)
+        s.sendall(bytes(file_ext.encode()))
+        for i in range(len(file_data)):
+            if i % len(hosts) == host:
+                s.sendall(file_data[i])
