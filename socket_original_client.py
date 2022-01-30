5e10d391be073b7e783e58fb3777a87914159ab5
Raghav-Srinivasan0 <raghavsrinivasan0@gmail.com>
2022-01-30 16:42:58 -0500

    not working fully yet

--- socket_original_client.py
+++ socket_original_client.py
@@ -11,24 +11,22 @@ my_parser.add_argument("PORT", metavar="port", type=int, help="The port")
 
 args = my_parser.parse_args()
 
-hosts = [
-    "192.168.1.3"
-    # , "127.0.0.1"
-]
+hosts = ["192.168.1.3", "192.168.1.43"]
 
 # HOST = args.HOST
 PORT = args.PORT
 
+f = FileDialogue()
+file_path = f.get_file_path()
+file_data = f.get_file_data()
+file_ext = f.get_file_ext()
+f.close()
+os.chdir("C:")
+os.remove(file_path)
+
 for host in range(len(hosts)):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((hosts[host], PORT))
-        f = FileDialogue()
-        file_path = f.get_file_path()
-        file_data = f.get_file_data()
-        file_ext = f.get_file_ext()
-        f.close()
-        os.chdir("C:")
-        os.remove(file_path)
         s.sendall(bytes(file_ext.encode()))
         for i in range(len(file_data)):
             if i % len(hosts) == host:
