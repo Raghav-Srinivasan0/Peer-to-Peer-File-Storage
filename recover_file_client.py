464daaf82612c2fcc9d49db059d16d784f5a0bf5
Raghav-Srinivasan0 <raghavsrinivasan0@gmail.com>
2022-02-03 12:15:43 -0500

    basic interpreter for .ins file

--- recover_file_client.py
+++ recover_file_client.py
@@ -7,3 +7,20 @@ data = f.get_file_data()
 ext = f.get_file_ext()
 path = f.get_file_path()
 f.close()
+
+port = -1
+hosts = []
+
+with open(path,'r+') as f:
+    data = f.readlines()
+    for line in range(len(data)):
+        if data[line].startswith('PORT: '):
+            port = int(data[line][data[line].index(' ')+1:])
+        if data[line].startswith('HOSTS: '):
+            list_str = data[line][data[line].index(' ')+1:].replace('[','').replace(']','').replace('\'','')
+            list_init = list_str.split(',')
+            hosts = list_init
+
+print(port)
+print(hosts)
+
