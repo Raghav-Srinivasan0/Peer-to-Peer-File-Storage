a75ccf2c9d907f9aa360ea9a8bf4fa40e0f67e6d
Raghav-Srinivasan0 <raghavsrinivasan0@gmail.com>
2022-02-10 11:19:26 -0500

    recover file system updates NOT WORKING

--- recover_file_client.py
+++ recover_file_client.py
@@ -1,11 +1,10 @@
 import socket
 import sys
-import multiprocessing
 from open_file_dialogue_test import FileDialogue
 
 # get instruction file
 path = ""
-while path.find(".ins") == -1:
+while path.find('.ins')==-1:
     try:
         f = FileDialogue()
         data = f.get_file_data()
@@ -13,26 +12,55 @@ while path.find(".ins") == -1:
         path = f.get_file_path()
         f.close()
     except FileNotFoundError:
-        print("Pick a file!")
+        print('Pick a file!')
         pass
 
 port = -1
 hosts = []
 
-with open(path, "r+") as f:
+with open(path,'r+') as f:
     data = f.readlines()
     for line in range(len(data)):
-        if data[line].startswith("PORT: "):
-            port = int(data[line][data[line].index(" ") + 1 :])
-        if data[line].startswith("HOSTS: "):
-            list_str = (
-                data[line][data[line].index(" ") + 1 :]
-                .replace("[", "")
-                .replace("]", "")
-                .replace("'", "")
-            )
-            list_init = list_str.split(",")
+        if data[line].startswith('PORT: '):
+            port = int(data[line][data[line].index(' ')+1:])
+        if data[line].startswith('HOSTS: '):
+            list_str = data[line][data[line].index(' ')+1:].replace('[','').replace(']','').replace('\'','')
+            list_init = list_str.split(',')
             hosts = list_init
 
 print(port)
 print(hosts)
+
+data_arr = []
+
+for host in range(len(hosts)):
+    arr = []
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        while True:
+            try:
+                s.connect((hosts[host],port-1))
+                break
+            except Exception as e:
+                print("Quitting because: " + str(e))
+                pass
+        try:
+            index_of_slash = path.rindex('/')
+        except Exception as e:
+            index_of_slash = path.rindex('\\')
+        file_name = path[index_of_slash+1:].replace(ext,'')
+        s.sendall(('**SEND**' + file_name).encode())
+        '''
+        s.bind((str(hosts[host]),port))
+        s.listen(1)
+        conn, addr = s.accept()
+        with conn:
+        '''
+        while True:
+            data = s.recv(1024)
+            if not data:
+                break
+            arr.append(data)
+    data_arr.append(arr)
+
+print(data_arr)
+        
---
+++ recover_file_server.py
@@ -0,0 +1,36 @@
+from multiprocessing import Process
+import multiprocessing
+import socket
+
+PATH = "D:/Destination_For_File-Transfer/"
+HOST = 'localhost'
+PORT = 4999
+NUMCONN = 5
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST,PORT))
+print("listening ...")
+s.listen(NUMCONN)
+
+def main_function(conn):
+    print("start of process")
+    with conn:
+        data = conn.recv(1024)
+        if not str(data).find('**SEND**') == -1:
+            filename = str(data)[str(data).find('**SEND**')+8:]
+    try:
+        print(filename)
+    except Exception as e:
+        pass
+
+while True:
+    try:
+        conn, addr = s.accept()
+    except Exception as e:
+        pass
+    for i in range(NUMCONN):
+        process = Process(target=main_function, args=(conn,))
+        process.start()
+        process.join()
+
